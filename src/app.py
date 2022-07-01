512981812df124012183dbce79f48b4a4c5faa68
Martin <martin996@hotmail.com.ar>
2022-07-01 18:18:06 -0300

    utlimos cambios

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, flash
 from flaskext.mysql import MySQL
+from pymysql.cursors import DictCursor
 from datetime import datetime
 import os
 
@@ -11,27 +12,28 @@ app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'mtd010202'
 app.config['MYSQL_DATABASE_DB'] = 'empleados'
+app.config['SECRET_KEY'] = "codoacodo"
 
-UPLOADS = os.path.join('src/uploads')
+UPLOADS = os.path.join('src/uploads')#guardamos la ruta como valor en la app
 app.config['UPLOADS'] = UPLOADS
 
 mysql.init_app(app)
 connection = mysql.connect()
-cursor = connection.cursor()
+cursor = connection.cursor(cursor=DictCursor) 
 
 def query_my_sql(query, datos = None, tipo_de_retorno = ""):
     if (datos != None):
         cursor.execute(query, datos)
     else:
         cursor.execute(query)
+
     if (tipo_de_retorno == "one"):
-        registro = cursor.fetchone()[0]
-        connection.commit()
+        registro = cursor.fetchone()
         return registro
-    if (tipo_de_retorno == "all"):
+    elif (tipo_de_retorno == "all"):
         registro = cursor.fetchall()
-        connection.commit()
         return registro
+
     connection.commit()
 
 @app.route('/foto_usuario/<path:nombre_foto>') 
@@ -44,35 +46,34 @@ def index():
     empleados = query_my_sql(sql, None, "all")
     return render_template('empleados/index.html', empleados = empleados)
 
-@app.route('/create')
-def create():
-    return render_template('empleados/create.html')
-    
-#procesa info del form
-@app.route('/store', methods=["POST"])
-def store():
-    _nombre = request.form['txtNombre']
-    _correo = request.form['txtCorreo']
-    _foto = request.files['txtFoto']
+@app.route('/empleado/crear', methods=["GET", "POST"])
+def alta_empleado():
+    if (request.method == "GET"):
+        return render_template('empleados/create.html')
+    if (request.method == "POST"):
+        _nombre = request.form['txtNombre']
+        _correo = request.form['txtCorreo']
+        _foto = request.files['txtFoto']
+        if (_nombre == "" or _correo == "" or _foto.filename == ""):
+            flash("Todos los campos son obligatorios.")
+            return redirect(url_for('alta_empleado'))
+        now = datetime.now()
+        tiempo = now.strftime("%Y%H%M%S")
 
-    now = datetime.now()
-    print(now)
-    tiempo = now.strftime("%Y%H%M%S")
+        if (_foto.filename != ''):
+            nuevoNombreFoto = tiempo + '_' + _foto.filename
+            _foto.save("src/uploads/" + nuevoNombreFoto)
 
-    if (_foto.filename != ''):
-        nuevoNombreFoto = tiempo + '_' + _foto.filename
-        _foto.save("src/uploads/" + nuevoNombreFoto)
+        sql = "INSERT INTO empleados (nombre, correo, foto) values (%s, %s, %s);"
+        datos = (_nombre, _correo, nuevoNombreFoto)
 
-    sql = "INSERT INTO empleados (nombre, correo, foto) values (%s, %s, %s);"
-    datos = (_nombre, _correo, nuevoNombreFoto)
-    
-    query_my_sql(sql, datos)
-    return redirect('/')
+        query_my_sql(sql, datos)
+        return redirect('/')
 
 @app.route('/delete/<int:id>')
 def delete(id):
     sql = "SELECT foto FROM empleados where id=%s"
-    nombre_foto = query_my_sql(sql, (id, ), "one")
+    nombre_foto = query_my_sql(sql, (id, ), "one")["foto"]
     try:
         os.remove(os.path.join(app.config['UPLOADS'], nombre_foto))
     except:
@@ -85,9 +86,7 @@ def delete(id):
 @app.route('/modify/<int:id>')
 def modify(id):
     sql = "SELECT * FROM empleados WHERE id=%s;"
-    cursor.execute(sql, id)
-    empleado = cursor.fetchone()
-    connection.commit()
+    empleado = query_my_sql(sql, (id, ), "one")
     return render_template('empleados/edit.html', empleado=empleado)
 
 @app.route('/update', methods=['POST'])
@@ -106,22 +105,17 @@ def update():
         _foto.save("src/uploads/" + nuevo_nombre_foto)
 
         sql = "SELECT foto FROM empleados WHERE id=%s;"
-        cursor.execute(sql, id)
-        connection.commit()
-
-        nombre_foto = cursor.fetchone()[0]
+        nombre_foto = query_my_sql(sql, (id, ), "one")["foto"]
+        print(nombre_foto)
         try:
             os.remove(os.path.join(app.config['UPLOADS'], nombre_foto))
         except:
             pass
         sql = "UPDATE empleados SET foto=%s WHERE id=%s;"
-        cursor.execute(sql, (nuevo_nombre_foto, id))
-        connection.commit()
+        query_my_sql(sql, (nuevo_nombre_foto, id))
 
     sql = "UPDATE empleados SET nombre=%s, correo=%s WHERE id=%s;"
-    cursor.execute(sql, datos)
-    connection.commit()
-
+    query_my_sql(sql, datos)
     return redirect('/')
 
 if __name__ == '__main__':
